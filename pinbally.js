a7db6a85c0a501a22c6217d204b889184b1e8be3
Johnny So <jso123450@gmail.com>
2016-03-04 12:33:28 -0500

    HEY WE STARTED

---
+++ pinbally.js
@@ -0,0 +1,24 @@
+/* ----------------- JAVASCRIPT ----------------- */
+
+console.log("Hello!@!@! I'm Pinball");
+
+/* ----------------- PINBALL-Y ----------------- */
+
+// Variables
+var fakeCanvas = document.getElementById("fakeCanvas");
+
+var width = fakeCanvas.width;
+var height = fakeCanvas.height;
+
+var intervalID;
+
+// startGame
+
+var startButton = document.getElementById("startButton");
+
+var startGame = function(e){
+    e.preventDefault();
+    // make ball in middle
+    // make pins
+    // YEAH
+};
