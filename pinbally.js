fa68f3d77116cbdd05d3004f7fd19d86bc9a5c3c
Johnny So <jso123450@gmail.com>
2016-03-07 12:30:56 -0500

    Added setup; need to work out how we will make every pin on the ball rotate after it hits it

--- pinbally.js
+++ pinbally.js
@@ -2,23 +2,71 @@
 
 console.log("Hello!@!@! I'm Pinball");
 
-/* ----------------- PINBALL-Y ----------------- */
+/* ----------------- PINBALLY ----------------- */
 
 // Variables
 var fakeCanvas = document.getElementById("fakeCanvas");
 
-var width = fakeCanvas.width;
-var height = fakeCanvas.height;
+var width = fakeCanvas.getAttribute("width");
+var height = fakeCanvas.getAttribute("height");
 
 var intervalID;
+var svgNSID = "http://www.w3.org/2000/svg";
 
 // startGame
 
 var startButton = document.getElementById("startButton");
 
+function setupBall(){
+    // setup ball
+    var ball = document.createElementNS( svgNSID, "circle" );
+    var cx = width/2;
+    var cy = height/2;
+    var radius = 100;
+
+    ball.setAttribute( "id", "ball" );
+    ball.setAttribute( "cx", cx);
+    ball.setAttribute( "cy", cy );
+    ball.setAttribute( "r", radius );
+    ball.setAttribute( "fill", "gray" );
+    ball.setAttribute( "stroke", "black" );
+    fakeCanvas.appendChild( ball );
+};
+
+function setupPin(){
+    // setup pin
+    var pin = document.createElementNS( svgNSID, "rect" );
+    var pinHeight = 100;
+    var pinWidth = 10;
+    var ball = document.getElementById("ball");
+    var cx = parseInt(ball.getAttribute("cx"));
+    var cy = parseInt(ball.getAttribute("cy"));
+    var radius = parseInt(ball.getAttribute("r"));
+
+    pin.setAttribute( "x", cx-(pinWidth/2) );
+    pin.setAttribute( "y", cy+radius+100 );
+    pin.setAttribute( "width", pinWidth );
+    pin.setAttribute( "height", pinHeight );
+    pin.setAttribute( "fill", "red" );
+    pin.setAttribute( "stroke", "black" );
+    fakeCanvas.appendChild( pin );
+};
+
+var clear = function(){
+    while (fakeCanvas.lastChild)
+	fakeCanvas.removeChild(fakeCanvas.lastChild);
+};
+
 var startGame = function(e){
     e.preventDefault();
-    // make ball in middle
-    // make pins
-    // YEAH
+    // setup
+    setupBall();
+    setupPin();
+
+    // rotation
+    var animateCode(){
+	
+    };
 };
+
+startButton.addEventListener( "click", startGame );
